cc24e256ed28ada7c219f20d320696c24c5b13c2
Carson Full <carsonfull@gmail.com>
2016-03-31 13:52:06 -0500

    Refactored Get and GetAll commands for search() method.

--- src/Console/Command/GetAllCommand.php
+++ src/Console/Command/GetAllCommand.php
@@ -2,90 +2,38 @@
 
 namespace CredStash\Console\Command;
 
-use CredStash\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * GetAll Command.
  *
  * @author Carson Full <carsonfull@gmail.com>
  */
-class GetAllCommand extends BaseCommand
+class GetAllCommand extends GetCommand
 {
     /**
      * {@inheritdoc}
      */
     protected function configure()
     {
+        parent::configure();
+
+        // Remove credential argument
+        $args = $this->getDefinition()->getArguments();
+        unset($args['credential']);
+        $this->getDefinition()->setArguments($args);
+
         $this
             ->setName('getall')
             ->setDescription('Get all credentials from the store')
-            ->addOption('format', 'f', InputOption::VALUE_REQUIRED, 'Output format. json, yaml, or csv.', 'json')
-            ->addOption(
-                'cred-version',
-                'c',
-                InputOption::VALUE_REQUIRED,
-                'Get a specific version of the credential <comment>[default: latest version]</comment>'
-            )
         ;
-        $this->addContextArgument();
-        parent::configure();
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function fetchCredentials(InputInterface $input, array $context, $version)
     {
-        $format = $input->getOption('format');
-        $accepted = ['json', 'yaml', 'csv'];
-        if (!in_array($format, $accepted)) {
-            throw new \InvalidArgumentException(
-                sprintf('Invalid format: "%s". Valid formats are %s.', $format, implode(', ', $accepted))
-            );
-        }
-
-        $credentials = $this->getCredStash()->getAll($input->getArgument('context'), $input->getOption('cred-version'));
-
-        if ($format === 'json') {
-            $out = json_encode($credentials, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-        } elseif ($format === 'csv') {
-            $out = $this->dumpCsv($credentials);
-        } else {
-            if (!class_exists(Yaml::class)) {
-                throw new RuntimeException(
-                    "YAML format requires Symfony's YAML component to be installed.\ncomposer require symfony/yaml"
-                );
-            }
-
-            $out = Yaml::dump($credentials);
-        }
-
-        $output->writeln($out);
-    }
-
-    /**
-     * Dumps key/value pair credentials to two column csv rows.
-     *
-     * @param array $data
-     *
-     * @return string
-     */
-    protected function dumpCsv(array $data)
-    {
-        $fh = fopen('php://temp', 'rw');
-
-        foreach ($data as $key => $value) {
-            fputcsv($fh, [$key, $value]);
-        }
-        rewind($fh);
-
-        $csv = stream_get_contents($fh);
-        fclose($fh);
-
-        return $csv;
+        return $this->getCredStash()->getAll($context, $version);
     }
 }
--- src/Console/Command/GetCommand.php
+++ src/Console/Command/GetCommand.php
@@ -2,10 +2,12 @@
 
 namespace CredStash\Console\Command;
 
+use CredStash\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Get Command.
@@ -14,6 +16,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class GetCommand extends BaseCommand
 {
+    protected static $VALID_FORMATS = ['json', 'yaml', 'csv'];
+
     /**
      * {@inheritdoc}
      */
@@ -25,8 +29,8 @@ class GetCommand extends BaseCommand
             ->addArgument(
                 'credential',
                 InputArgument::REQUIRED,
-                "The name of the credential to get. Using the wildcard character\n" .
-                '<comment>"*"</comment> will search for credentials that match the pattern.'
+                "The name of the credential to get. Using the wildcard characters\n" .
+                '<comment>"*"</> or <comment>"?"</> will search for credentials that match the pattern.'
             )
             ->addOption(
                 'cred-version',
@@ -40,6 +44,13 @@ class GetCommand extends BaseCommand
                 null,
                 'Don\'t append newline to returned value (useful in scripts or with binary files)'
             )
+            ->addOption(
+                'format',
+                'f',
+                InputOption::VALUE_REQUIRED,
+                "Output format for multiple credentials.\nValid formats: " . implode(', ', static::$VALID_FORMATS),
+                'json'
+            )
         ;
         $this->addContextArgument();
         parent::configure();
@@ -48,13 +59,118 @@ class GetCommand extends BaseCommand
     /**
      * {@inheritdoc}
      */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        parent::initialize($input, $output);
+
+        $format = $input->getOption('format');
+        if (!in_array($format, static::$VALID_FORMATS)) {
+            throw new \InvalidArgumentException(
+                sprintf('Invalid format: "%s". Valid formats are %s.', $format, implode(', ', static::$VALID_FORMATS))
+            );
+        }
+
+        if ($format === 'yaml' && !class_exists(Yaml::class)) {
+            throw new RuntimeException(
+                "YAML format requires Symfony's YAML component to be installed.\ncomposer require symfony/yaml"
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $value = $this->getCredStash()->get($input->getArgument('credential'), $input->getArgument('context'));
+        $context = $input->getArgument('context');
+        $version = $input->getOption('cred-version');
+
+        $value = $this->fetchCredentials($input, $context, $version);
+
         $output->write($value);
 
         if (!$input->getOption('no-break')) {
             $output->writeln('');
         }
+    }
+
+    /**
+     * Fetch credentials from given input.
+     * 
+     * Context and version are passed in as they are always required.
+     *
+     * @param InputInterface $input
+     * @param array          $context
+     * @param string|null    $version
+     *
+     * @return string
+     */
+    protected function fetchCredentials(InputInterface $input, array $context, $version)
+    {
+        $name = $input->getArgument('credential');
+        $credStash = $this->getCredStash();
+
+        // If $name contains these wildcard characters call search() instead of get()
+        $search = false;
+        $wildcards = ['*', '?', '['];
+        foreach ($wildcards as $wildcard) {
+            if (strpos($wildcard, $name) !== false) {
+                $search = true;
+                break;
+            }
+        }
+
+        if (!$search) {
+            return $credStash->get($name, $context, $version);
+        }
+
+        $credentials = $credStash->search($name, $context, $version);
+
+        return $this->dumpCredentials($input, $credentials);
+    }
+
+    /**
+     * Dump given credentials to a string based on "format" option from input given.
+     *
+     * @param InputInterface $input
+     * @param array          $credentials
+     *
+     * @return string
+     */
+    protected function dumpCredentials(InputInterface $input, array $credentials)
+    {
+        $format = $input->getOption('format');
+
+        if ($format === 'json') {
+            return json_encode($credentials, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }
+
+        if ($format === 'csv') {
+            return $this->dumpCsv($credentials);
+        }
+
+        return Yaml::dump($credentials);
+    }
+
+    /**
+     * Dumps key/value pair credentials to two column csv rows.
+     *
+     * @param array $data
+     *
+     * @return string
+     */
+    protected function dumpCsv(array $data)
+    {
+        $fh = fopen('php://temp', 'rw');
+
+        foreach ($data as $key => $value) {
+            fputcsv($fh, [$key, $value]);
+        }
+        rewind($fh);
+
+        $csv = stream_get_contents($fh);
+        fclose($fh);
+
+        return $csv;
     }
 }
