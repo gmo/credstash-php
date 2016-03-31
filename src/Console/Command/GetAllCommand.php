664b5bb51f66567054bbf07ef4186162ebd9f7fd
Carson Full <carsonfull@gmail.com>
2016-03-31 00:29:09 -0500

    Implemented getAll command. Requiring (dev) symfony/yaml for yaml dumping.

--- src/Console/Command/GetAllCommand.php
+++ src/Console/Command/GetAllCommand.php
@@ -2,6 +2,12 @@
 
 namespace CredStash\Console\Command;
 
+use CredStash\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+
 /**
  * GetAll Command.
  *
@@ -14,10 +20,72 @@ class GetAllCommand extends BaseCommand
      */
     protected function configure()
     {
-        parent::configure();
         $this
             ->setName('getall')
             ->setDescription('Get all credentials from the store')
+            ->addOption('format', 'f', InputOption::VALUE_REQUIRED, 'Output format. json, yaml, or csv.', 'json')
+            ->addOption(
+                'cred-version',
+                'c',
+                InputOption::VALUE_REQUIRED,
+                'Get a specific version of the credential <comment>[default: latest version]</comment>'
+            )
         ;
+        $this->addContextArgument();
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $format = $input->getOption('format');
+        $accepted = ['json', 'yaml', 'csv'];
+        if (!in_array($format, $accepted)) {
+            throw new \InvalidArgumentException(
+                sprintf('Invalid format: "%s". Valid formats are %s.', $format, implode(', ', $accepted))
+            );
+        }
+
+        $credentials = $this->getCredStash()->getAll($input->getArgument('context'), $input->getOption('cred-version'));
+
+        if ($format === 'json') {
+            $out = json_encode($credentials, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        } elseif ($format === 'csv') {
+            $out = $this->dumpCsv($credentials);
+        } else {
+            if (!class_exists(Yaml::class)) {
+                throw new RuntimeException(
+                    "YAML format requires Symfony's YAML component to be installed.\ncomposer require symfony/yaml"
+                );
+            }
+
+            $out = Yaml::dump($credentials);
+        }
+
+        $output->writeln($out);
+    }
+
+    /**
+     * Dumps key/value pair credentials to two column csv rows.
+     *
+     * @param array $data
+     *
+     * @return string
+     */
+    protected function dumpCsv(array $data)
+    {
+        $fh = fopen('php://temp', 'rw');
+
+        foreach ($data as $key => $value) {
+            fputcsv($fh, [$key, $value]);
+        }
+        rewind($fh);
+
+        $csv = stream_get_contents($fh);
+        fclose($fh);
+
+        return $csv;
     }
 }
